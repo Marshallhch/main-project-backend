c75a69bd326a8c3e59ffe3bdfcde8f6184ac63cb
Marshallhch <marshallhch@gmail.com>
2022-12-20 12:50:49 +0900

    admin page init

--- model/register.php
+++ model/register.php
@@ -84,7 +84,7 @@
       if(!$pwd_valid){
         echo json_encode(array("login_msg" => "비밀번호가 틀립니다."));
       } else {
-        session_start();
+        
         $_SESSION['userid'] = $userid;
         echo json_encode(array("userid" => $_SESSION['userid']));
       }
@@ -100,6 +100,7 @@
     if(isset($_SESSION['userid'])){
       session_unset();
       session_destroy();
+      echo json_encode(array("userid" => "guest"));
     }
   }
 ?>
